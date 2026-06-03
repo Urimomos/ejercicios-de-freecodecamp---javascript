978eb278904cb28e658c2e5cbbf9c49eb4d7125a
Uriel Alejandro <uafj22@gmail.com>
2026-06-02 21:26:42 -0600

    avances de funcion

--- bucles/Build a Smart Pantry Restocker.js
+++ bucles/Build a Smart Pantry Restocker.js
@@ -20,6 +20,21 @@ function parseShipment(rawData){
     return matrizDeObjetos;
 }
 
+function planRestock(pantry, shipment){
+    let arregloResultado = [];
+    for(let ship of shipment){
+        if(ship.qty <= 0){
+            arregloResultado.push(
+                {
+                    type: "discard",
+                    item : ship
+                }
+            );
+        }
+    }
+    return arregloResultado;
+}
+
 const rawData = [
   "A10|Tomatoes|5|2027-01-01",        // no zone field
   "B21|Bananas|10|2027-01-01|fridge", // zone: "fridge"
@@ -27,4 +42,9 @@ const rawData = [
   "C32|Eggs|3|2027-01-01|pantry"    // zone: "pantry"
 ];
 
-console.log(parseShipment(rawData));
+const pantry = [{ sku: "D43", name: "Pineapples", qty: 2, expires: "2027-01-01", zone: "general" }];
+const shipment = [{ sku: "D43", name: "Pineapples", qty: 0, expires: "2027-01-01", zone: "general" },
+                  { sku: "E54", name: "Peppers", qty: -1, expires: "2027-01-01", zone: "fridge" }
+                ];
+
+console.log(planRestock(pantry, shipment));
