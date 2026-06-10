d81226da110b5032481eecc4db066f3f86236887
Uriel Alejandro <uafj22@gmail.com>
2026-06-09 22:14:26 -0600

    convertidor terminado

--- Revision De Fundamentos de Javascript/Implementa un Convertidor de Entidades HTML.js
+++ Revision De Fundamentos de Javascript/Implementa un Convertidor de Entidades HTML.js
@@ -2,11 +2,32 @@ function convertHTML(cadena){
     const arregloLetras = cadena.split("");
     for(let letra = 0; letra < arregloLetras.length; letra++){
         let codigoAscii = arregloLetras[letra].charCodeAt(0);
-        console.log(codigoAscii);
         if(codigoAscii == 34 || codigoAscii == 38 || codigoAscii == 60 || codigoAscii == 62 || codigoAscii == 39){
-            arregloLetras.splice(letra, );
+            switch(codigoAscii){
+                case 34:{
+                    arregloLetras.splice(letra,1,"&quot;");
+                    break;
+                }
+                case 38:{
+                    arregloLetras.splice(letra,1,"&amp;");
+                    break;
+                }
+                case 39:{
+                    arregloLetras.splice(letra,1,"&apos;");
+                    break;
+                }
+                case 60:{
+                    arregloLetras.splice(letra,1,"&lt;");
+                    break;
+                }
+                case 62:{
+                    arregloLetras.splice(letra,1,"&gt;");
+                    break;
+                }
+            }
         }
     }
+    return arregloLetras.join("");
 }
 
-console.log(convertHTML("Dolce & Gabbana"));
+console.log(convertHTML('Stuff in "quotation marks"'));
